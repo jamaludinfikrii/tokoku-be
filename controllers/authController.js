22fb795dc650bd3c88b83067dcac93ffe0390b47
fikri <me@fikri.tech>
2020-06-04 06:32:17 +0700

    auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,12 @@
 const db = require('./../database/mysql')
 const login = (req,res) => {
     const data = req.body
+    console.log(data)
     const sql = 'select * from users where username = ? and password = ?;'
     db.query(sql , [data.username,data.password], (err,result) => {
         try {
             if(err) throw err
+            console.log(result)
             res.send(result)
         } catch (error) {
             console.log(error)
