d7693cbeb9d9da9c04b0cf8676b0905680a746c4
fikri <me@fikri.tech>
2020-06-04 11:13:14 +0700

    test notif

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const db = require('./../database/mysql')
+require('dotenv').config()
+
 const login = (req,res) => {
     const data = req.body
     console.log(data)
@@ -14,4 +16,50 @@ const login = (req,res) => {
     })
 }
 
-module.exports = {login}
+const testNotif = (request,response) => {
+    var sendNotification = function(data) {
+        var headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": "Basic " + process.env.API_KEY_ONE_SIGNAL
+        };
+        
+        var options = {
+            host: "onesignal.com",
+            port: 443,
+            path: "/api/v1/notifications",
+            method: "POST",
+            headers: headers
+        };
+        
+        var https = require('https');
+        var req = https.request(options, function(res) {  
+            res.on('data', function(data) {
+            console.log("Response:");
+            console.log(JSON.parse(data));
+            });
+        });
+        
+        req.on('error', function(e) {
+            console.log("ERROR:");
+            console.log(e);
+        });
+        
+        req.write(JSON.stringify(data));
+        response.send('Message sent')
+        req.end();
+        
+    };
+    
+    var message = { 
+        app_id: process.env.APP_ID_ONE_SIGNAL,
+        contents: {"en": "Nikmati belanja Sepuasnya di tokoku"},
+        headings : {"en" : "Promo Lebaran"},
+        included_segments: ["All"]
+    };
+    
+    sendNotification(message);
+}
+
+
+
+module.exports = {login , testNotif}
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,6 @@
 const Router = require('express').Router()
 const contr = require('./../controllers/authController')
 Router.post('/login',contr.login)
+Router.post('/test-notif',contr.testNotif)
 
 module.exports = Router
